ccc8c1794b4fbe5a9b3c3e933a7fc4217bd6a681
Roman Sherstiuk <vampirs.alucard@gmail.com>
2024-12-21 20:26:32 +0300

    added navbar

--- todo_manager/todo_manager/settings.py
+++ todo_manager/todo_manager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l!tz_lcwvdkljsr^vd1x=kl)(ylxfqq)957#(=i3en*=g85szi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
